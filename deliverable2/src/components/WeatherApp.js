834ecccb53b696a1ae657c5d5e817e20552bb08b
Raizen-7 <ferminyanes07@gmail.com>
2022-05-09 00:25:22 -0500

    button logic missing

--- deliverable2/src/components/WeatherApp.js
+++ deliverable2/src/components/WeatherApp.js
@@ -3,21 +3,30 @@ import axios from 'axios';
 
 const WeatherApp = () => {
 
-    const [weather, setWeather] = useState({})
-    
-
+    const [weather, setWeather] = useState({});
+    const [temperature, setTemperature] = useState(0);
+    const [isCelcius, setIsCelcius] = useState(true);
+;
 
     useEffect(() => {
+        //función para la geolocalización
         function success(pos) {
-
+            //se actualizo a let
             let crd = pos.coords;
           
             console.log('Your current position is:');
             console.log(`Latitude : ${crd.latitude}`);
             console.log(`Longitude: ${crd.longitude}`);
             console.log(`More or less ${crd.accuracy} meters.`);
+            //consumo de API
             axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=a32d0570e649c4eac8be3c53a9b3af30
-            `).then(res => setWeather(res.data))
+            `).then((res) => {
+                setWeather(res.data);
+            setTemperature(res.data.main?.temp);
+                console.log(res.data)
+
+            })
+
           }
           
           function error(err) {
@@ -29,16 +38,23 @@ const WeatherApp = () => {
 
         }, [])
         
-        const temCelcius =  `${weather.main?.temp -273.15} "°C"`;
-        // const temFahrenheit = `${temCelcius * 9/5 + 32} "°F"` ;
+        const changeTem = () => {
+            if (isCelcius) {
+              // Convertir a grados Celcius
+              setWeather(temperature - 273.15);
+              setIsCelcius(false);
+            } else {
+              // Convertir a fahrenheit
+              setWeather(isCelcius * 9/5 + 32);
+              setIsCelcius(true);
+            }
+          };
+       
+    
 
-        const temMaxCelcius =  `${weather.main?.temp_max -273.15} "°C"`;
-        const temMinCelcius =  `${weather.main?.temp_min -273.15} "°C"`;
-        // const temMaxFahrenheit = `${temMaxCelcius * 9/5 + 32} "°F"` ;
-        // const temMinFahrenheit = `${temMinCelcius * 9/5 + 32} "°F"` ;
-          
     
     return (
+        //JSX de la aplicación
         <div className="Conteiner">
             <header>
                 <h1>Weather App</h1> 
@@ -48,10 +64,11 @@ const WeatherApp = () => {
             </header>
 
             <div className='imgWeather'>
-            <img src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} alt="" />
-                <p>{temCelcius}</p>
-                <p>Temp Max: {temMaxCelcius} - Temp Min: {temMinCelcius} </p>
-                <div /*datos api */>
+                <div className='contentImg'>
+            <img className='cloud' src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} alt="" />
+                <p className='degees'> ${weather.main?.temp -273.15} {setIsCelcius ? "°C" : "°F"}</p>
+                </div>
+                <div className='ContentData' /*datos api */> 
                     <p> "{weather.weather?.[0].description}" </p>
                     <p> Wind Speed: {weather.wind?.speed} m/s </p>
                     <p>Clouds: {weather.clouds?.all}% </p>
@@ -59,7 +76,7 @@ const WeatherApp = () => {
                 </div>
             </div>     
             <div>
-                <button>Degees °F/°C</button>
+                <button onClick={changeTem}>Degees °F/°C</button>
             </div>
         </div>
     );
